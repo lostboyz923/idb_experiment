ff340ea681b29b1658ca2227bff3fc2aec11a324
kenwoo9y <ei.1px.9.01@gmail.com>
2020-05-30 22:55:49 +0900

    define file upload variables

--- js/fileInput.js
+++ js/fileInput.js
@@ -1,5 +1,14 @@
 $('.custom-file-input').on('change',function(){
     $(this).next('.custom-file-label').html($(this)[0].files[0].name);
+    //ファイルの取得
+    file = $(this).prop('files')[0];
+    console.log(file);
+    //リサイズ後の高さと幅の最大値を定義
+    const maxWidth = 500;
+    const maxHeight = 500;
+    //画像のリサイズ
+    let image = new Image();
+    let fileReader = new FileReader();
 })
 //ファイルの取消
 $('.reset').click(function(){
