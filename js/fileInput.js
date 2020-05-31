235740874fe6bc0099ea4fc9b822fa5a5c04a2c5
kenwoo9y <ei.1px.9.01@gmail.com>
2020-05-31 23:36:23 +0900

    draw image

--- js/fileInput.js
+++ js/fileInput.js
@@ -2,6 +2,7 @@ $('.custom-file-input').on('change',function(){
     $(this).next('.custom-file-label').html($(this)[0].files[0].name);
     //ファイルの取得
     file = $(this).prop('files')[0];
+    resizeImage(file);
 })
 //ファイルの取消
 $('.reset').click(function(){
@@ -11,6 +12,7 @@ $('.reset').click(function(){
 
 // 画像のリサイズ
 function resizeImage(file) {
+    console.log(file);
     //リサイズ後の幅と高さの最大値を定義
     const maxWidth = 500;
     const maxHeight = 500;
@@ -22,6 +24,8 @@ function resizeImage(file) {
             // 元画像の幅と高さ
             let width = image.width;
             let height = image.height;
+            console.log('before width:'+width);
+            console.log('before height:'+height);
             //画像が1MBより大きければリサイズ対象とする
             if(file.size > 1024 * 1024 * 1) {
                 if(width > height) {
@@ -36,8 +40,22 @@ function resizeImage(file) {
                     height = maxHeight;
                 }
             }
+            //canvas
+            let canvas = $('#canvas').attr('width', width).attr('height', height);
+            let ctx = canvas[0].getContext('2d');
+
+            //canvasに描画されている画像を削除
+            ctx.clearRect(0,0,width,height);
+
+            //画像を描画する
+            ctx.drawImage(image,0,0,image.width,image.height,0,0,width,height);
+            
+            console.log('after width:'+width);
+            console.log('after height:'+height);
         };
+        image.src = event.target.result;
     };
+    fileReader.readAsDataURL(file);
 }
 
 function ImageToBase64(image, type) {
